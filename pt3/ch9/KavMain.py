182039d7435341e5426923f18c24acd3c289eb81
Seongeun Yu <johong130801@gmail.com>
2018-06-28 23:01:18 +0900

    added additional data

---
+++ pt3/ch9/KavMain.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+class KavMain:
+    def init(self):
+        pass
+
+    def uninit(self):
+        pass
+
+    def scan(self):
+        pass
+
+    def disinfected(self):
+        pass
+
+    def viruslist(self):
+        pass
+
+    def getinfo(self):
+        pass
